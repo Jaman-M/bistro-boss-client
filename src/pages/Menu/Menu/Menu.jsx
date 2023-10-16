739cd0920f3d7624819d5aba28ad945082767078
Jaman-M <jaman.manik1@gmail.comm>
2023-10-17 05:48:15 +0800

    added  Dynamic page title by using React Helmet

--- src/pages/Menu/Menu/Menu.jsx
+++ src/pages/Menu/Menu/Menu.jsx
@@ -1,7 +1,12 @@
-
+import { Helmet } from 'react-helmet-async';
 const Menu = () => {
     return (
         <div>
+            <Helmet>
+                <title>Bistro Boss | Menu</title>
+                <link rel="canonical" href="https://www.tacobell.com/" />
+            </Helmet>
+
             <h2>This is menu</h2>
         </div>
     );
