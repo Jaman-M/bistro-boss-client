1f218d29100482257502d1df8aac51ef36db917b
Jaman-M <jaman.manik1@gmail.comm>
2023-11-13 04:44:38 +0800

    add cart icon from react icon, API naming convention and API to save cart

--- src/components/FoodCard/FoodCard.jsx
+++ src/components/FoodCard/FoodCard.jsx
@@ -1,7 +1,47 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { AuthContext } from '../../providers/AuthProvider';
+import Swal from 'sweetalert2';
+import { useNavigate } from 'react-router-dom';
 
-const FoodCard = ({item}) => {
-    const {name, image, price, recipe} = item;
+const FoodCard = ({ item }) => {
+    const { name, image, price, recipe } = item;
+
+    const { user } = useContext(AuthContext);
+    const navigate = useNavigate();
+
+    const handleAddToCart = item => {
+        console.log(item);
+        if (user) {
+            fetch('http://localhost:5000/carts')
+                .then(res => res.json())
+                .then(data => {
+                    if (data.insertedId) {
+                        Swal.fire({
+                            position: "top-end",
+                            icon: "success",
+                            title: "Your work has been saved",
+                            showConfirmButton: false,
+                            timer: 1500
+                        });
+                    }
+                })
+        }
+        else {
+            Swal.fire({
+                title: "Please Login to order the food",
+                icon: "warning",
+                showCancelButton: true,
+                confirmButtonColor: "#3085d6",
+                cancelButtonColor: "#d33",
+                confirmButtonText: "Login Now"
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    navigate('/login')
+                }
+            })
+        }
+
+    }
     return (
         <div className="card w-96 bg-orange-50 shadow-xl">
             <figure><img src={image} alt="Shoes" /></figure>
@@ -10,7 +50,7 @@ const FoodCard = ({item}) => {
                 <h2 className="card-title">{name}</h2>
                 <p>{recipe}</p>
                 <div className="card-actions justify-end">
-                    <button className="btn btn-outline border-0 border-b-4 mt-4 bg-slate-200 text-orange-400 border-orange-400">Add to Cart</button>
+                    <button onClick={() => handleAddToCart(item)} className="btn btn-outline border-0 border-b-4 mt-4 bg-slate-200 text-orange-400 border-orange-400">Add to Cart</button>
                 </div>
             </div>
         </div>
--- src/pages/Shared/NavBar/Navbar.jsx
+++ src/pages/Shared/NavBar/Navbar.jsx
@@ -1,6 +1,7 @@
 import { useContext } from "react";
 import { Link } from "react-router-dom";
 import { AuthContext } from "../../../providers/AuthProvider";
+import { FaShoppingCart } from 'react-icons/fa';
 
 const Navbar = () => {
     const { user, logOut } = useContext(AuthContext);
@@ -17,6 +18,14 @@ const Navbar = () => {
         <li><Link to="/order/salad">Order Food</Link></li>
         <li><Link to="/secret">Secret</Link></li>
         {/* <li><Link to="/login">Login</Link></li> */}
+        <li>
+            <Link to ="/">
+                <button className="btn gap-2">
+                <FaShoppingCart></FaShoppingCart>
+                    <div className="badge badge-secondary">+0</div>
+                </button>
+            </Link>
+        </li>
         {
             user ? <>
             <span>{user?.displayName}</span>
