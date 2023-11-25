0432366e47142f14714ee504af6f1fe1c70d3052
Jaman-M <jaman.manik1@gmail.comm>
2023-11-25 10:06:41 +0800

    display cart items to review before order

--- src/pages/Dashboard/MyCart/MyCart.jsx
+++ src/pages/Dashboard/MyCart/MyCart.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Helmet } from 'react-helmet-async';
 import useCart from '../../../hooks/useCart';
+import { FaTrashAlt } from 'react-icons/fa';
 
 const MyCart = () => {
     const [cart] = useCart();
@@ -12,10 +13,58 @@ const MyCart = () => {
                 <title>Bistro Boss | My Cart</title>
                 <link rel="canonical" href="https://www.tacobell.com/" />
             </Helmet>
-            <div className='uppercase'>
+            <div className='uppercase font-semibold flex justify-evenly items-center h-[60px]'>
                 <h3 className='text-3xl'>Total Items: {cart.length}</h3>
                 <h3 className='text-3xl'>Total Price: $ {total}</h3>
                 <button className='btn btn-warning btn-sm'>Pay</button>
+            </div>
+            <div className="overflow-x-auto">
+                <table className="table">
+                    {/* head */}
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Food</th>
+                            <th>Item Name</th>
+                            <th>Price</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {/* row 1 */}
+                        {
+                            cart.map((item, index) => <tr
+                                key={item._id}
+                            >
+                                <td>
+                                    {index + 1}
+                                </td>
+                                <td>
+                                    
+                                        <div className="avatar">
+                                            <div className="mask mask-squircle w-12 h-12">
+                                                <img src={item.image}/>
+                                            </div>
+                                        </div>
+                                        
+                                    
+                                </td>
+                                <td>
+                                    {item.name}
+                                </td>
+                                <td className='text-end'>$ {item.price}</td>
+                                <th>
+                                    <button className="btn btn-ghost btn-sm bg-red-400 text-white"><FaTrashAlt></FaTrashAlt></button>
+                                </th>
+                            </tr>)
+                        }
+
+
+
+                    </tbody>
+
+
+                </table>
             </div>
         </div>
     );
