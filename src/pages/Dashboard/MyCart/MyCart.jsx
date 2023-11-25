5a736341341509c8ad5d1a4dab081437e853d4c5
Jaman-M <jaman.manik1@gmail.comm>
2023-11-25 11:20:37 +0800

    delete cart item and sync data using refetch

--- src/pages/Dashboard/MyCart/MyCart.jsx
+++ src/pages/Dashboard/MyCart/MyCart.jsx
@@ -2,13 +2,47 @@ import React from 'react';
 import { Helmet } from 'react-helmet-async';
 import useCart from '../../../hooks/useCart';
 import { FaTrashAlt } from 'react-icons/fa';
+import Swal from 'sweetalert2';
 
 const MyCart = () => {
-    const [cart] = useCart();
+    const [cart, refetch] = useCart();
     console.log(cart);
-    const total = cart.reduce((sum, item) => item.price + sum, 0)
+
+    const total = cart.reduce((sum, item) => item.price + sum, 0);
+
+    const handleDelete = item => {
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+        }).then((result) => {
+            if (result.isConfirmed) {
+                fetch(`http://localhost:5000/carts/${item._id}`, {
+                    method: 'DELETE'
+                })
+                    .then(res => res.json())
+                    .then(data => {
+                        if (data.deletedCount > 0) {
+                            refetch();
+                            Swal.fire({
+                                title: "Deleted!",
+                                text: "Your file has been deleted.",
+                                icon: "success"
+                            });
+
+                        }
+                    })
+
+            }
+        })
+    }
+
     return (
-        <div>
+        <div className='w-5/6'>
             <Helmet>
                 <title>Bistro Boss | My Cart</title>
                 <link rel="canonical" href="https://www.tacobell.com/" />
@@ -40,21 +74,21 @@ const MyCart = () => {
                                     {index + 1}
                                 </td>
                                 <td>
-                                    
-                                        <div className="avatar">
-                                            <div className="mask mask-squircle w-12 h-12">
-                                                <img src={item.image}/>
-                                            </div>
+
+                                    <div className="avatar">
+                                        <div className="mask mask-squircle w-12 h-12">
+                                            <img src={item.image} />
                                         </div>
-                                        
-                                    
+                                    </div>
+
+
                                 </td>
                                 <td>
                                     {item.name}
                                 </td>
-                                <td className='text-end'>$ {item.price}</td>
+                                <td className=''>$ {item.price}</td>
                                 <th>
-                                    <button className="btn btn-ghost btn-sm bg-red-400 text-white"><FaTrashAlt></FaTrashAlt></button>
+                                    <button onClick={() => handleDelete(item)} className="btn btn-ghost btn-sm bg-red-400 text-white"><FaTrashAlt></FaTrashAlt></button>
                                 </th>
                             </tr>)
                         }
