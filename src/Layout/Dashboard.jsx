43675c901623691dece556ab2e423a7d2a3bb2ae
Jaman-M <jaman.manik1@gmail.comm>
2023-11-29 14:36:50 +0800

    load all users on the dashboard page, Display All users on the dashboard and also added Make admin optin also

--- src/Layout/Dashboard.jsx
+++ src/Layout/Dashboard.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import { NavLink, Outlet } from 'react-router-dom';
-import { FaShoppingCart, FaWallet, FaCalendarAlt, FaHome } from 'react-icons/fa';
+import { FaShoppingCart, FaWallet, FaCalendarAlt, FaHome, FaUtensils, FaUsers, FaBook } from 'react-icons/fa';
 import useCart from '../hooks/useCart';
 const Dashboard = () => {
     const [cart] = useCart();
+
+    // TODO: load data from the server to have dynamic isAdmin based on data
+    const isAdmin = true;
+
     return (
         <div className="drawer lg:drawer-open">
             <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
@@ -15,16 +19,32 @@ const Dashboard = () => {
             <div className="drawer-side bg-[#D1A054]">
                 <label htmlFor="my-drawer-2" aria-label="close sidebar" className="drawer-overlay"></label>
                 <ul className="menu p-4 w-80 min-h-full  text-base-content">
-                    {/* Sidebar content here */}
-                    <li><NavLink to="/dashboard/home"><FaHome></FaHome>Home</NavLink></li>
-                    <li><NavLink to="/dashboard/reservations"><FaCalendarAlt></FaCalendarAlt>Reservations</NavLink></li>
-                    <li><NavLink to="/dashboard/history"><FaWallet></FaWallet>Payment History</NavLink></li>
-                    <li>
-                        <NavLink to="/dashboard/mycart"><FaShoppingCart></FaShoppingCart> My Cart
-                        <div className="badge badge-secondary">+{cart?.length || 0}</div>
-                        </NavLink>
-                        
-                        </li>
+
+                    {
+                        isAdmin ? <>
+                            {/* Sidebar content here */}
+                            <li><NavLink to="/dashboard/home"><FaHome></FaHome>Admin Home</NavLink></li>
+                            <li><NavLink to="/dashboard/reservations"><FaUtensils></FaUtensils> Add Items</NavLink></li>
+                            <li><NavLink to="/dashboard/history"><FaWallet></FaWallet>Manage Items</NavLink></li>
+                            <li><NavLink to="/dashboard/history"><FaBook></FaBook>Manage Bookings</NavLink></li>
+                            <li><NavLink to="/dashboard/allusers"><FaUsers></FaUsers>All Users</NavLink></li>
+                            
+
+                        </> : <>
+                            {/* Sidebar content here */}
+                            <li><NavLink to="/dashboard/home"><FaHome></FaHome>Home</NavLink></li>
+                            <li><NavLink to="/dashboard/reservations"><FaCalendarAlt></FaCalendarAlt>Reservations</NavLink></li>
+                            <li><NavLink to="/dashboard/history"><FaWallet></FaWallet>Payment History</NavLink></li>
+                            <li>
+                                <NavLink to="/dashboard/mycart"><FaShoppingCart></FaShoppingCart> My Cart
+                                    <div className="badge badge-secondary">+{cart?.length || 0}</div>
+                                </NavLink>
+
+                            </li>
+                        </>
+                    }
+
+
                     <div className="divider"></div>
                     <li><NavLink to="/"><FaHome></FaHome>Home</NavLink></li>
                     <li><NavLink to="/menu">Menu</NavLink></li>
---
+++ src/pages/Dashboard/AllUsers/AllUsers.jsx
@@ -0,0 +1,102 @@
+// import { useQuery } from '@tanstack/react-query';
+// import React from 'react';
+
+// const AllUsers = () => {
+
+//     const { data: users = [], refetch } = useQuery(['users'], async () => {
+//         const res = await fetch('http://localhost:5000/users');
+//         return res.json();
+//     })
+
+//     return (
+//         <div>
+//             {users.length}
+//         </div>
+//     );
+// };
+
+// export default AllUsers;
+
+import { useQuery } from '@tanstack/react-query';
+import React from 'react';
+import { Helmet } from 'react-helmet-async';
+import { FaTrashAlt, FaUserShield } from 'react-icons/fa';
+import Swal from 'sweetalert2';
+
+const AllUsers = () => {
+
+    const { data: users = [], refetch } = useQuery({
+        queryKey: ['users'],
+        queryFn: async () => {
+            const res = await fetch('http://localhost:5000/users');
+            return res.json();
+        },
+    });
+
+    const handleMakeAdmin = user => {
+        fetch(`http://localhost:5000/users/admin/${user._id}`, {
+            method: 'PATCH'
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                if(data.modifiedCount){
+                    refetch();
+                    Swal.fire({
+                        position: "top-end",
+                        icon: "success",
+                        title: `${user.name} is an Admin now!`,
+                        showConfirmButton: false,
+                        timer: 1500
+                      });
+                }
+            })
+    }
+
+    const handleDelete = user => {
+
+    }
+
+    return (
+        <div className='w-5/6'>
+            <Helmet>
+                <title>Bistro boss | All Users</title>
+            </Helmet>
+            <h3 className="text-3xl font-semibold my-4">Total Users: {users.length}</h3>
+            <div className="overflow-x-auto">
+                <table className="table table-zebra">
+                    {/* head */}
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <td>Name</td>
+                            <td>Email</td>
+                            <td>Role</td>
+                            <td>Action</td>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            users.map((user, index) => <tr key={user._id}>
+                                <th>{index + 1}</th>
+                                <td>{user.name}</td>
+                                <td>{user.email}</td>
+                                <td>{
+                                    user.role == 'admin' ? 'admin' : <button onClick={() => handleMakeAdmin(user)} className="btn btn-ghost btn-sm bg-orange-400 text-white"><FaUserShield></FaUserShield></button>
+
+
+                                }</td>
+
+                                <td><button onClick={() => handleDelete(user)} className="btn btn-ghost btn-sm bg-red-400 text-white"><FaTrashAlt></FaTrashAlt></button></td>
+                            </tr>)
+                        }
+
+
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    );
+};
+
+export default AllUsers;
