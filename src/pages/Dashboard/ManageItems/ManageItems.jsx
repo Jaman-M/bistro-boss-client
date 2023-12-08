3177a9f599781bd2bc04c7c366e9a5f4a6283828
Jaman-M <jaman.manik1@gmail.comm>
2023-12-09 04:36:02 +0800

    Load all menuItems with real info, ;  delete menuItem with verify admin

--- src/pages/Dashboard/ManageItems/ManageItems.jsx
+++ src/pages/Dashboard/ManageItems/ManageItems.jsx
@@ -1,9 +1,43 @@
 import React from 'react';
 import SectionTitle from '../../../components/SectionTitle/SectionTitle';
 import useMenu from '../../../hooks/useMenu';
+import { FaTrashAlt } from 'react-icons/fa';
+import Swal from 'sweetalert2';
+import useAxiosSecure from '../../../hooks/useAxiosSecure';
 
 const ManageItems = () => {
-    const [menu] = useMenu();
+    const [menu, , refetch] = useMenu();
+    const axiosSecure = useAxiosSecure()
+    const handleDelete = item => {
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+        }).then((result) => {
+            if (result.isConfirmed) {
+
+                axiosSecure.delete(`/menu/${item._id}`)
+                    .then(res => {
+                        console.log('Deleted response', res.data);
+                        if (res.data.deletedCount > 0) {
+                            refetch();
+                            Swal.fire({
+                                title: "Deleted!",
+                                text: "Your file has been deleted.",
+                                icon: "success"
+
+                            });
+                        }
+                        
+                    })
+            }
+        });
+
+    }
     return (
         <div className='w-5/6'>
             <SectionTitle heading="Manage All Items" subHeading="Hurry Up"></SectionTitle>
@@ -12,26 +46,20 @@ const ManageItems = () => {
                     {/* head */}
                     <thead>
                         <tr>
-                            <th>
-                                <label>
-                                    <input type="checkbox" className="checkbox" />
-                                </label>
-                            </th>
-                            <th>Name</th>
-                            <th>Job</th>
-                            <th>Favorite Color</th>
+                            <th>#</th>
+                            <th>Item</th>
+                            <th>Category</th>
+                            <th>Price</th>
                             <th>Update</th>
                             <th>Delete</th>
                         </tr>
                     </thead>
                     <tbody>
                         {
-                            menu.map(item => <tr key={item._id}>
-                                <th>
-                                    <label>
-                                        <input type="checkbox" className="checkbox" />
-                                    </label>
-                                </th>
+                            menu.map((item, index) => <tr key={item._id}>
+                                <td>
+                                    {index + 1}
+                                </td>
                                 <td>
                                     <div className="flex items-center gap-3">
                                         <div className="avatar">
@@ -40,22 +68,22 @@ const ManageItems = () => {
                                             </div>
                                         </div>
                                         <div>
-                                            <div className="font-bold">Hart Hagerty</div>
+                                            <div className="font-bold">{item.name}</div>
 
                                         </div>
                                     </div>
                                 </td>
                                 <td>
-                                    Zemlak, Daniel and Leannon
-                                    <br />
-                                    <span className="badge badge-ghost badge-sm">Desktop Support Technician</span>
+                                    {item.category}
                                 </td>
-                                <td>Purple</td>
                                 <td>
-                                    <button className="btn btn-ghost btn-xs">details</button>
+                                    $ {item.price}
                                 </td>
                                 <td>
                                     <button className="btn btn-ghost btn-xs">details</button>
+                                </td>
+                                <td>
+                                    <button onClick={() => handleDelete(item)} className="btn btn-ghost btn-sm bg-red-400 text-white"><FaTrashAlt></FaTrashAlt></button>
                                 </td>
                             </tr>)
                         }
