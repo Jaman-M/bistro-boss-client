df1bfc217d3950212d13277eb573dfa77b7dc6d3
Jaman-M <jaman.manik1@gmail.comm>
2023-12-09 16:00:20 +0800

    create payment route

---
+++ src/pages/Dashboard/Payment/Payment.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import SectionTitle from '../../../components/SectionTitle/SectionTitle';
+
+const Payment = () => {
+    return (
+        <div className='w-5/6'>
+            <SectionTitle subHeading="Please Process" heading="Payment"></SectionTitle>
+            <h2 className='text-3xl'>Taka payment----</h2>
+        </div>
+    );
+};
+
+export default Payment;
