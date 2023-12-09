590d4c612a116adbfce3be1d8f8430c76bdd9f37
Jaman-M <jaman.manik1@gmail.comm>
2023-12-10 00:02:21 +0800

    install stripe, create card element and use publishable key

--- src/pages/Dashboard/Payment/Payment.jsx
+++ src/pages/Dashboard/Payment/Payment.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import SectionTitle from '../../../components/SectionTitle/SectionTitle';
+import CheckoutForm from './CheckoutForm';
+import { loadStripe } from '@stripe/stripe-js';
+import { Elements } from '@stripe/react-stripe-js';
+
+// TODO: provide publishable key
+const stripePromise = loadStripe('import.meta.env.VITE_Payment_Gateway_PK')
 
 const Payment = () => {
     return (
         <div className='w-5/6'>
             <SectionTitle subHeading="Please Process" heading="Payment"></SectionTitle>
             <h2 className='text-3xl'>Taka payment----</h2>
+            <Elements stripe={stripePromise}>
+                <CheckoutForm></CheckoutForm>
+            </Elements>
+
         </div>
     );
 };
