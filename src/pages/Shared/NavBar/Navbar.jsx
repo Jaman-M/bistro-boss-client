d8b0015fefdc8b34e09a67e00df6433a17782c05
Jaman-M <jaman.manik1@gmail.comm>
2023-12-17 14:25:38 +0800

    Create different Dashboard home page for User and Admin

--- src/pages/Shared/NavBar/Navbar.jsx
+++ src/pages/Shared/NavBar/Navbar.jsx
@@ -3,8 +3,10 @@ import { Link } from "react-router-dom";
 import { AuthContext } from "../../../providers/AuthProvider";
 import { FaShoppingCart } from 'react-icons/fa';
 import useCart from "../../../hooks/useCart";
+import useAdmin from "../../../hooks/useAdmin";
 const Navbar = () => {
     const { user, logOut } = useContext(AuthContext);
+    const [isAdmin] = useAdmin();
     const [cart] = useCart();
 
     const handleLogOut = () => {
@@ -18,7 +20,10 @@ const Navbar = () => {
         <li><Link to="/">Home</Link></li>
         <li><Link to="/menu">Our Menu</Link></li>
         <li><Link to="/order/salad">Order Food</Link></li>
-        <li><Link to="/secret">Secret</Link></li>
+        {/* <li><Link to="/secret">Secret</Link></li> */}
+        {
+            isAdmin ? <li><Link to="/dashboard/adminhome">Dashboard</Link></li> : <li><Link to="/dashboard/userhome">Dashboard</Link></li>
+        }
         {/* <li><Link to="/login">Login</Link></li> */}
         <li>
             <Link to="/dashboard/mycart">
